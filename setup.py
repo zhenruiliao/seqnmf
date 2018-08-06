c24dd2c8da264ef8fe9c6a2bffe2cbe0cd9b818a
Jeremy Manning <jeremy@hypercube.local>
2018-08-05 21:36:06 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
-readme = 'TODO: write longer description'
-license = 'MIT'
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
 
 setup(
-    name='seqNMF',
+    name='seqnmf',
     version='0.1.0',
     description='Python implementation of seqNMF',
     long_description=readme,
@@ -13,6 +18,6 @@ setup(
     author_email='contextualdynamics@gmail.com',
     url='https://www.context-lab.com',
     license=license,
+    install_requires = requirements,
     packages=find_packages(exclude=('tests', 'docs'))
 )
-
